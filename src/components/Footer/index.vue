d2a6bc41728f0861dca649add3b3b3682468d5a7
liuxiangxiang <1431510710@qq.com>
2020-06-05 14:42:38 +0800

    vue

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name:'Footer',
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/pages/Register/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'Register',
+}
+</script>
+
+<style scoped>
+
+
+</style>
